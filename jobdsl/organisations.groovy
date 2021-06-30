9645fa020c3c7f49f12a1cc6ba2f3b8e463a3a46
John O'Neill <johnoneill546@gmail.com>
2021-06-30 13:55:27 +0100

    Add PIP and VH to organisation list

--- jobdsl/organisations.groovy
+++ jobdsl/organisations.groovy
@@ -9,6 +9,8 @@ private boolean isSandbox() {
 
 List<Map> orgs = [
         [name: 'Platform', regex: '(cnp-toffee.*)'],
+        [name: 'PIP', displayName: 'Publishing & Information Hub', regex: '(pip-.*|pih-.*)'],
+        [name: 'VH', displayName: 'Video Hearings', regex: '(vh.*)'],
 ]
 
 orgs.each { Map org ->
